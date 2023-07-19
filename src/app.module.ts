38c84c11ba85c3641b11d92da5054f81fbb18740
Xhuljo Balli <xh.balliu@codevider.com>
2023-07-20 00:07:40 +0200

    added configuration layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import configuration from 'config/configuration';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
